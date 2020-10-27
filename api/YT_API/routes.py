42109411bbfd508e2c2559e1572e010f03b5df5d
Reza <ojyarn2602@gmail.com>
2020-10-27 12:59:58 +0330

    new route added

--- api/YT_API/routes.py
+++ api/YT_API/routes.py
@@ -1,12 +1,14 @@
 from YT_API import app
 from flask import request, jsonify
 from YT_API.get_info import YouTube
+from YT_API.yt_dl import yt_link_finder
 
 def send_res(req):
-    if (req):
+    if req:
         response = req.fetch_data()
         return jsonify(response)
     return jsonify(message='invalid id/username')
+
 
 @app.route('/info')
 def info():
@@ -18,3 +20,8 @@ def info():
     except:
         pass
     return send_res(req)
+
+@app.route('/youtube/download')
+def download():
+    pagelink = request.form.get('pagelink')
+    return jsonify(yt_link_finder(pagelink))
