08b4128219089ef6b247ed112f2ce73944e07fb2
AlirezaYeganeh <AlirezaYeganeh41@yahoo.com>
2020-11-14 23:26:15 +0330

    better structure

--- client/src/components/form.js
+++ client/src/components/form.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
-import { httpClient } from './utils/http'
+import { httpClient } from '../utils/http'
 
 export default () => {
   const { register, handleSubmit } = useForm()
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Form from './form';
+import Form from '../components/form';
 
 export default () => (
   <div className="container">
