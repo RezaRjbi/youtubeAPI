7c64fc0286ea6dd51661aef42dbe38f9bd11368b
AlirezaYeganeh <AlirezaYeganeh41@yahoo.com>
2020-11-14 23:07:31 +0330

    change api routes to post method

--- api/YT_API/routes.py
+++ api/YT_API/routes.py
@@ -5,7 +5,7 @@ from YT_API.utils import send_res
 from YT_API.yt_dl import VideoObj
 
 
-@app.route('/info')
+@app.route('/info', methods=['POST'])
 def info():
     form = request.form
     pagelink = form.get('pagelink')
@@ -17,13 +17,14 @@ def info():
     return jsonify(response)
 
 
-@app.route('/youtube/download')
+@app.route('/youtube/download', methods=['POST'])
 def download():
     pagelink = request.form.get('pagelink')
     video = VideoObj(pagelink)
     _info = video.yt_link_finder()
     response_dict = VideoObj.as_dict(_info)
     return jsonify(response_dict)
+
 
 @app.route('/test')
 def test():
